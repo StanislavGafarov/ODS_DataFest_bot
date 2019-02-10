148d2d2a756d19943eb56c87c44a9d803adba165
sgafarov <sgafarov@gazprom-media.tech>
2019-02-10 18:29:30 +0300

    added check email, can spam, table_sheet.  ConversationHandler was rewritten.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
                           ConversationHandler)
-from config import BOT_TOKKEN
+from config import BOT_TOKKEN, APPROVED_EMAIL_LIST
 
 import logging
 
@@ -11,69 +11,53 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-GENDER, PHOTO, LOCATION, BIO = range(4)
+CHOOSING = 0
+CHECK_EMAIL, LOCATION, BIO = range(3)
 
 
-def start(bot, update):
-    reply_keyboard = [['Boy', 'Girl', 'Other']]
+def menu(bot, update):
+    reply_keyboard = [['Проверить Email', 'Расписание','Подписаться на обновления']]
+    # reply_keyboard = [['Boy', 'Girl', 'Other']]
 
     update.message.reply_text(
-        'Hi! My name is Professor Bot. I will hold a conversation with you. '
-        'Send /cancel to stop talking to me.\n\n'
-        'Are you a boy or a girl?',
+        'Привет, че как? '
+        'Че делать будем?',
         reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    return CHOOSING
 
-    return GENDER
 
-
-def gender(bot, update):
-    user = update.message.from_user
-    logger.info("Gender of %s: %s", user.first_name, update.message.text)
-    update.message.reply_text('I see! Please send me a photo of yourself, '
-                              'so I know what you look like, or send /skip if you don\'t want to.',
+def check_email(bot, update):
+    # user = update.message.from_user
+    # logger.info('')
+    update.message.reply_text('Please type your email, or send /skip if you don\'t want to',
                               reply_markup=ReplyKeyboardRemove())
+    email = update.message.text
+    if email in APPROVED_EMAIL_LIST:
+        update.message.reply_text('Fuck yeah, you are in!')
+    else:
+        update.message.reply_text('Go away looser, you are not in the list')
+    return CHOOSING
 
-    return PHOTO
+
+def table_sheet(bot, update):
+    update.message.reply_text('{}'.format('there will be spread sheet with timing'))
+    return CHOOSING
 
 
-def photo(bot, update):
+def can_spam(bot, update):
     user = update.message.from_user
-    photo_file = bot.get_file(update.message.photo[-1].file_id)
-    photo_file.download('user_photo.jpg')
-    logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
-    update.message.reply_text('Gorgeous! Now, send me your location please, '
-                              'or send /skip if you don\'t want to.')
-
-    return LOCATION
+    chat_id = bot.get_updates()[-1].message.chat_id
+    logger.info('User:{}, Chat_id:{} subscribed for notification'.format(user.first_name,chat_id))
+    update.message.reply_text('Thank for sub, bro')
+    return CHOOSING
 
 
-def skip_photo(bot, update):
+def skip_email(bot, update):
     user = update.message.from_user
-    logger.info("User %s did not send a photo.", user.first_name)
-    update.message.reply_text('I bet you look great! Now, send me your location please, '
-                              'or send /skip.')
+    logger.info("User %s did not send an email.", user.first_name)
+    update.message.reply_text('You seem a bit paranoid!')
 
-    return LOCATION
-
-
-def location(bot, update):
-    user = update.message.from_user
-    user_location = update.message.location
-    logger.info("Location of %s: %f / %f", user.first_name, user_location.latitude,
-                user_location.longitude)
-    update.message.reply_text('Maybe I can visit you sometime! '
-                              'At last, tell me something about yourself.')
-
-    return BIO
-
-
-def skip_location(bot, update):
-    user = update.message.from_user
-    logger.info("User %s did not send a location.", user.first_name)
-    update.message.reply_text('You seem a bit paranoid! '
-                              'At last, tell me something about yourself.')
-
-    return BIO
+    return CHOOSING
 
 
 def bio(bot, update):
@@ -107,18 +91,12 @@ def main():
 
     # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
+        entry_points=[CommandHandler('start', menu)],
 
         states={
-            GENDER: [RegexHandler('^(Boy|Girl|Other)$', gender)],
-
-            PHOTO: [MessageHandler(Filters.photo, photo),
-                    CommandHandler('skip', skip_photo)],
-
-            LOCATION: [MessageHandler(Filters.location, location),
-                       CommandHandler('skip', skip_location)],
-
-            BIO: [MessageHandler(Filters.text, bio)]
+            CHOOSING: [RegexHandler('^(Проверить Email)$', check_email),
+                   RegexHandler('^(Расписание)$', table_sheet),
+                   RegexHandler('^(Подписаться на обновления)$', can_spam)]
         },
 
         fallbacks=[CommandHandler('cancel', cancel)]
