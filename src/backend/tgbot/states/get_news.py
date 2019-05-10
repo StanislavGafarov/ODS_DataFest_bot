ee13830c93710c642b8cd21b5a9bb5f177fcecc1
sgafarov <sgafarov@gazprom-media.tech>
2019-05-10 23:32:19 +0300

    test4

--- src/backend/tgbot/states/get_news.py
+++ src/backend/tgbot/states/get_news.py
@@ -1,6 +1,6 @@
 from telegram.ext import run_async, MessageHandler, Filters, CallbackQueryHandler
-# from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove, ReplyKeyboardMarkup
-# from django.core.paginator import Paginator
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove, ReplyKeyboardMarkup
+from django.core.paginator import Paginator
 
 from backend.tgbot.tghandler import TGHandler
 from backend.tgbot.base import TelegramBotApi
@@ -27,49 +27,49 @@ class GetNews(TGHandler):
         update.message.reply_text(TEXT_NEWS_UNSUBSCRIBED, reply_markup=self.define_keyboard(user))
         return self.MAIN_MENU
 
-    # @Decorators.composed(run_async, Decorators.save_msg, Decorators.with_user)
-    # def show_news(self, api: TelegramBotApi, user: TGUser, update):
-    #     logger.disable(logger.WARNING)
-    #     text = update.message.text
-    #     logger.info('User {} have chosen {} '.format(user, text))
-    #     # сначала последние
-    #     news_list = News.objects.filter(target_group__in=[NewsGroup.all_users(), NewsGroup.news_subscription()]).order_by('-id')
-    #     news_pages = Paginator(news_list, 1)
-    #     page = news_pages.get_page(1)
-    #
-    #     for news in page:
-    #         send = news.get_sender()
-    #         send(api, user.tg_id, self.get_kb(page))
-    #     return self.GET_NEWS
-    #
-    # def get_kb(self, page):
-    #     keyboard = []
-    #     if page.has_previous():
-    #         next_news = InlineKeyboardButton("⬅️", callback_data=f"page?{page.previous_page_number()}")
-    #         keyboard.append(next_news)
-    #
-    #     if page.has_next():
-    #         next_news = InlineKeyboardButton("➡️", callback_data=f"page?{page.next_page_number()}")
-    #         keyboard.append(next_news)
-    #     if keyboard:
-    #         return InlineKeyboardMarkup([keyboard])
-    #     return None
-    #
-    # def show_news_inline(self, api: TelegramBotApi, update):
-    #     logger.disable(logger.WARNING)
-    #     query = update.callback_query
-    #     page_no = int(query.data.split("?")[1])
-    #
-    #     news_list = News.objects.filter(
-    #         target_group__in=[NewsGroup.all_users(), NewsGroup.news_subscription()]).order_by('-id')
-    #     news_pages = Paginator(news_list, 1)
-    #     page = news_pages.get_page(page_no)
-    #
-    #     api.bot.delete_message(query.message.chat_id, query.message.message_id)
-    #
-    #     for news in page:
-    #         send = news.get_sender()
-    #         send(api, query.message.chat_id, self.get_kb(page))
+    @Decorators.composed(run_async, Decorators.save_msg, Decorators.with_user)
+    def show_news(self, api: TelegramBotApi, user: TGUser, update):
+        logger.disable(logger.WARNING)
+        text = update.message.text
+        logger.info('User {} have chosen {} '.format(user, text))
+        # сначала последние
+        news_list = News.objects.filter(target_group__in=[NewsGroup.all_users(), NewsGroup.news_subscription()]).order_by('-id')
+        news_pages = Paginator(news_list, 1)
+        page = news_pages.get_page(1)
+
+        for news in page:
+            send = news.get_sender()
+            send(api, user.tg_id, self.get_kb(page))
+        return self.GET_NEWS
+
+    def get_kb(self, page):
+        keyboard = []
+        if page.has_previous():
+            next_news = InlineKeyboardButton("⬅️", callback_data=f"page?{page.previous_page_number()}")
+            keyboard.append(next_news)
+
+        if page.has_next():
+            next_news = InlineKeyboardButton("➡️", callback_data=f"page?{page.next_page_number()}")
+            keyboard.append(next_news)
+        if keyboard:
+            return InlineKeyboardMarkup([keyboard])
+        return None
+
+    def show_news_inline(self, api: TelegramBotApi, update):
+
+        query = update.callback_query
+        page_no = int(query.data.split("?")[1])
+
+        news_list = News.objects.filter(
+            target_group__in=[NewsGroup.all_users(), NewsGroup.news_subscription()]).order_by('-id')
+        news_pages = Paginator(news_list, 1)
+        page = news_pages.get_page(page_no)
+
+        api.bot.delete_message(query.message.chat_id, query.message.message_id)
+
+        for news in page:
+            send = news.get_sender()
+            send(api, query.message.chat_id, self.get_kb(page))
 
     def create_state(self):
         state = {self.GET_NEWS: [
--- src/backend/tgbot/utils.py
+++ src/backend/tgbot/utils.py
@@ -34,7 +34,7 @@ def check_certs():
                     'Not valid certificates: issued to "{}", but host ip is "{}"'.format(issued_to, settings.HOST_IP))
 
 
-logger = logging.getLogger('bot')
+logger = logging.disable(logging.WARNING).getLogger('bot')
 
 
 class Decorators(object):
