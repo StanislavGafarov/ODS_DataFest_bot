0a8f57c79e33a6da3664ae567a9d4715f96a4795
sgafarov <sgafarov@gazprom-media.tech>
2019-05-11 04:30:02 +0300

    back utils

--- src/backend/tgbot/utils.py
+++ src/backend/tgbot/utils.py
@@ -34,7 +34,7 @@ def check_certs():
                     'Not valid certificates: issued to "{}", but host ip is "{}"'.format(issued_to, settings.HOST_IP))
 
 
-logger = logging.getLogger('bot').setLevel(logging.ERROR)
+logger = logging.getLogger('bot')
 
 
 class Decorators(object):
